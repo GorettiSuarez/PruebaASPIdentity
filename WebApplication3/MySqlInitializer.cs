89a9fbe5a602a54c9ab4da7751627a73c211d9af
Goretti Suarez <goretti__sr.95@hotmail.es>
2018-03-15 13:45:15 +0000

    Agregar archivos de proyecto.

---
+++ WebApplication3/MySqlInitializer.cs
@@ -0,0 +1,34 @@
+﻿using WebApplication3.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace WebApplication3
+{
+    public class MySqlInitializer : IDatabaseInitializer<AppUsersDbContext>
+    {
+        public void InitializeDatabase(AppUsersDbContext context)
+        {
+            if (!context.Database.Exists())
+            {
+                // if database did not exist before - create it
+                context.Database.Create();
+            }
+            else
+            {
+                // query to check if MigrationHistory table is present in the database
+                var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
+                string.Format(
+                  "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{0}' AND table_name = '__MigrationHistory'",
+                  "apiusersdb"));
+
+                // if MigrationHistory table is not there (which is the case first time we run) - create it
+                if (migrationHistoryTableExists.FirstOrDefault() == 0)
+                {
+                    context.Database.Delete();
+                    context.Database.Create();
+                }
+            }
+        }
+    }
+}
